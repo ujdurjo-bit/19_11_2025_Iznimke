c54559618e0998177d0b02e76fdc4eef4ad75a69
Josip <ujdur.jo@gmail.com>
2025-11-19 18:45:23 +0100

    Novi commit

--- src/DjeljenjesNulom.java
+++ src/DjeljenjesNulom.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class DjeljenjesNulom {
+
+    public static void  main (String[] args) {
+        int x = 9;
+        int y = 0;
+
+        try {
+            int rezultat = x / y;
+            System.out.println(rezultat);
+        } catch (Throwable e) {
+            System.err.println("Iznimka: " + e.getMessage());
+        } finally {
+            System.out.println("Kalkulacija gotova.");
+        }
+    }
 }
---
+++ src/KlasaError.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class KlasaError {
+    public static void main(String[] args) {
+        try {
+            ArrayList<String> znakovniNiz = new ArrayList<>();
+            while (true) {
+                znakovniNiz.add("Znakovni dodatak");
+            }
+        } catch (OutOfMemoryError e) {
+            System.err.println("Greška" + e.getMessage());
+        }
+    }
+}
